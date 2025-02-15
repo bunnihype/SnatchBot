7023fdd32b43da11336085e91110c9571ee39d45
bunnihype <nyameye222@gmail.com>
2025-02-15 04:49:35 -1200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Created and prgrammed my snatch", "snatch is him frfr"];
+const statusMessages = ["Created and prgrammed by snatch", "snatch is him frfr"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
